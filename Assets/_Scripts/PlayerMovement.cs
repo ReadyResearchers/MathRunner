cb2c7649fe15cdd71321cbeb06c54751cb9265b2
Peter <>
2023-04-03 00:30:38 -0400

    adding player movement,animations and the player

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveS = 5f;
+    public Animator animator;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveS * Time.fixedDeltaTime);
+    }
+}
